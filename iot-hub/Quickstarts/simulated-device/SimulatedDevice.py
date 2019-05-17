447305536035e6c755c8759bb30f70aef41cb207
Francisco Trejo <trejo@Franciscos-MacBook-Air.local>
2019-05-16 20:40:01 -0600

    conexion arduino

--- iot-hub/Quickstarts/simulated-device/SimulatedDevice.py
+++ iot-hub/Quickstarts/simulated-device/SimulatedDevice.py
@@ -68,3 +68,22 @@ if __name__ == '__main__':
     print ( "IoT Hub Quickstart #1 - Simulated device" )
     print ( "Press Ctrl-C to exit" )
     iothub_client_telemetry_sample_run()
+
+
+# 
+#
+#
+#     from time import sleep
+# import serial
+# ser = serial.Serial('/dev/cu.usbmodem1411', 9600) # Establish the connection on a specific port
+# counter = 32 # Below 32 everything in ASCII is gibberish
+# while True:
+#     # counter +=1
+#     # ser.write(str(chr(counter))) # Convert the decimal number to ASCII then send it to the Arduino
+#     a = ser.readline() # Read the newest output from the Arduino
+#     print a
+#     sleep(.1) # Delay for one tenth of a second
+#     # if counter == 255:
+#     #     counter = 32
+#
+#
